103c88edd9efa0cecc05d464151fb729024bab10
Mathis Dedial <mathis@dedial.net>
2018-02-14 13:12:42 +0100

    Add webpack-dev-server config

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # currenTcy
 BIOTS 2018 hackathon project
+
+## Technology used
+* mithril
+* web3
+* truffle
+
+## Usage
+* `npm start`: Starts the webpack dev server
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
 const path = require('path');
 
 module.exports = {
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    watchContentBase: true,
+  },
   entry: './frontend/index.js',
   output: {
     filename: 'bundle.js',
