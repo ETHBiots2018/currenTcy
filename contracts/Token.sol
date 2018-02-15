2161c3b5d37b10465fa6538da62264a51c455a2d
Claudio Fanconi <fanconic@student.ethz.ch>
2018-02-15 10:18:40 +0100

    created Modifier smartmeter only

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -7,12 +7,19 @@ pragma solidity ^0.4.4;
 // - Security concerns like race conditions, etc.: https://solidity.readthedocs.io/en/develop/security-considerations.html
 
 contract Token {
+  /*
+   * Implementation of global variables, as well as mapping
+   */
+
   // Ledger
   mapping(address => uint256) public balanceOf;
 
   // Map smart meters to their owners
   // TODO: This needs to be a mapping of sort (address => array(address)) so that a smart meter can be owned by multiple users
   mapping(address => address) public user;
+
+  // map a boolean true, if a smartmeter is registered
+  mapping(address => bool) registered;
 
   // Implicit constructor to set ownership  
   address public owner = msg.sender;
@@ -23,25 +30,45 @@ contract Token {
     _;
   }
 
-  // For functions which can only be performed by smart meters
+  // For functions which can only be performed by smartmeters
   modifier smartMeterOnly {
-    // TODO
+    require(registered[msg.sender] == true);
     _;
   }
 
-  // Wh -> Token conversion
-  function whToToken (uint256 wh) public pure returns (uint256) {
-    // As of right now, this performs a 1:1 conversion of Wh to tokens
-    // May be adapted in future implementations
-    return wh;
+
+/* 
+ * Functions which are only allowed to be executed by the owner (EWZ).
+ * These functions include: setting and getting a smartmeter to a user,
+ * as well as registering and unregistering smartmeters, for the smartMeterOnly modifier
+ */
+
+  // set smartmeter user(s)
+  function setUser (address _smartMeter, address _user) public ownerOnly {
+    user[_smartMeter] = _user;
   }
 
-  // Token -> Wh conversion
-  function tokenToWh (uint256 token) public pure returns (uint256) {
-    // As of right now, this performs a 1:1 conversion of Wh to tokens
-    // May be adapted in future implementations
-    return token;
+  // get smartmeter user(s)
+  function getUser (address _smartMeter) public view ownerOnly returns (address) {
+    return user[_smartMeter];
   }
+
+  // register smartmeter
+  function regSmartMeter (address _smartMeter) public ownerOnly {
+    registered[_smartMeter] = true;
+  }
+
+  // unregister smartmeter
+  function unregSmartMeter(address _smartMeter) public ownerOnly {
+   registered[_smartMeter] = false; 
+  }
+
+
+  /*
+   * The following functions are only for the smartmeters
+   * they are for production and consumption of electricity,
+   * and the token balance management of each user
+   */
 
   // Credit the User tokens for Wh produced
   function produce (uint256 wh) public smartMeterOnly {
@@ -63,14 +90,23 @@ contract Token {
     return consumed;
   }
 
-  // set smart meter user(s)
-  function setUser (address _smartMeter, address _user) public ownerOnly {
-    user[_smartMeter] = _user;
+
+  /*
+   * Following functions are only mathematical functions for the token <-> wh conversion
+   */
+
+    // Wh -> Token conversion
+  function whToToken (uint256 wh) public pure returns (uint256) {
+    // As of right now, this performs a 1:1 conversion of Wh to tokens
+    // May be adapted in future implementations
+    return wh;
   }
 
-  // get smart meter user(s)
-  function getUser (address _smartMeter) public view ownerOnly returns (address) {
-    return user[_smartMeter];
+  // Token -> Wh conversion
+  function tokenToWh (uint256 token) public pure returns (uint256) {
+    // As of right now, this performs a 1:1 conversion of Wh to tokens
+    // May be adapted in future implementations
+    return token;
   }
 
 }
