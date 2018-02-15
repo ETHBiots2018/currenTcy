5a2051d600d941b48f7d0d8d5d6a8e71318b427c
Simon Andreas Bösch <simon-andreas.boesch@hotmail.com>
2018-02-15 17:35:10 +0100

    Connect user with smart meter

--- frontend/views/register.js
+++ frontend/views/register.js
@@ -13,10 +13,22 @@ export default class Meter {
       m('form', [
         m('.form-group', [
           m('label[for=address]'),
-          m('input.form-control#address', { id: 'address' }),
+          m('input.form-control#address', { id: 'address', placeholder: 'Enter smart meter adress' }),
         ]),
         m('button.btn#registerButton', { onclick: Model.register.bind(this.model) }, 'Register'),
         m('button.btn#unregisterButton', { onclick: Model.unregister.bind(this.model) }, 'Unregister'),
+      ]),
+      m('h1', 'Connect user to smart meter'),
+      m('form', [
+        m('.form-group', [
+          m('label[for=connectSmartMeter]'),
+          m('input.form-control#connectSmartMeter', { id: 'connectSmartMeter', placeholder:'Enter smart meter address' }),
+        ]),
+        m('.form-group', [
+          m('label[for=connectUser]'),
+          m('input.form-control#connectUser', { id: 'connectUser', placeholder:'Enter user address'}),
+        ]),
+        m('button.btn#connectButton', { onclick: Model.connect.bind(this.model) }, 'Connect')
       ]),
     ]);
   }
